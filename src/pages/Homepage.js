e79736687cff01d10a67bd8ced47ac814d75a67d
DJsankalp <balannavar.sankalp@gmail.com>
2022-11-11 16:05:19 +0530

    routing v3 checkpoint

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,15 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Query from './Query';
+
 function Homepage() {
     return (
-    
-    <h2>This is homepage</h2>
 
+        <body>
+            
+
+            <h2>This is homepage</h2>
+        </body>
     );
 }
 export default Homepage;
---
+++ src/pages/Query.js
@@ -0,0 +1,8 @@
+function Query() {
+    return (
+
+    <h2>This is infomation page</h2>
+
+    );
+}
+export default Query;
