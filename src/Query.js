173d82aaca53ae7df4bf3cc8efb4fdde4d7bc216
DJsankalp <balannavar.sankalp@gmail.com>
2022-11-17 12:43:07 +0530

    work on 17-nov

--- src/Query.js
+++ src/Query.js
@@ -6,7 +6,25 @@ function Query() {
 
         <body>
             <h2>This is Query page</h2>
-            
+
+        <div >
+            <div>
+                Select categories
+            </div>
+
+            <div>
+                Post query
+            </div>
+        </div>
+
+            <footer class='footer navbar-fixed-bottom'>
+                <div class='row'>
+                    <div class='col-sm-4'>Developed by: Sankalp, Soumya, Pranavi, Shri Nagahari</div>
+                    <div class='col-sm-4'></div>
+                    <div class='col-sm-4'>KLE Technological University, Hubli - 580030</div>
+                </div>
+
+            </footer>
         </body >
     );
 }
--- src/Register.js
+++ src/Register.js
@@ -5,27 +5,65 @@ import './Register.css';
 
 function Register() {
     function validate() {
-        
-        var username, password, p,q,r,s, status, i;
+
+        var username, usn, p, q, r, s, t, status, i;
 
         p = document.getElementById('username').value;
         q = document.getElementById('password1').value;
+        t = document.getElementById('password2').value;
         r = document.getElementById('branch').value;
         s = document.getElementById('usn').value;
 
-        axios.post("http://localhost:3002/api/post", 
-        {
-            username: p,
-            password: q,
-            branch: r,
-            usn: s
-        })
-        .then(function (response) {
-            alert(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        if (t == q) {
+            axios.get("http://localhost:3002/api/get")
+                .then(function (response) {
+                    for (i = 0; i < response.data.length; i++) {
+                        username = response.data[i].username;
+                        usn = response.data[i].usn;
+
+                        if (p == username && s == usn) {
+                            status = 0;
+                            alert("User already exits. dd");
+                        }
+                        else {
+                            status = 1;
+                        }
+                    }
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });
+
+            if (status == 0) {
+                alert("User already exits.");
+            }
+            else if (status == 1) {
+                alert("Registration successful");
+                axios.post("http://localhost:3002/api/post",
+                    {
+                        username: p,
+                        password: q,
+                        branch: r,
+                        usn: s
+                    })
+                    .then(function (response) {
+                        alert(response);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }
+
+
+        }
+
+        else {
+            alert("Passwords do not match.");
+        }
 
     }
     return (
@@ -40,20 +78,20 @@ function Register() {
                 <form class='register-form' onSubmit={validate}>
                     <div className="form-group h4">
                         <label>Username: </label>
-                        <input type='text' class="form-control" id='username' required/> 
+                        <input type='text' class="form-control" id='username' required />
                         {/* pattern="[A-Za-z]{3}" */}
                         <br />
                         <label>Password: </label>
-                        <input type='password' class="form-control" id='password1' required/>
+                        <input type='password' class="form-control" id='password1' required />
                         <br />
                         <label>Re enter Password: </label>
-                        <input type='password' class="form-control" id='password2' required/>
+                        <input type='password' class="form-control" id='password2' required />
                         <br />
                         <label>Branch: </label>
-                        <input type='text' class="form-control" id='branch' required/>
+                        <input type='text' class="form-control" id='branch' required />
                         <br />
                         <label>USN: </label>
-                        <input type='text' class="form-control" id='usn' required/>
+                        <input type='text' class="form-control" id='usn' required />
                         <br /><br />
                         <input type='submit' value='Signup' class='btn btn-default' />
                     </div>
